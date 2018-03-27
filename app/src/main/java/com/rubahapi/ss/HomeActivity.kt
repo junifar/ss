e652d423389c04dad5b4340225790cdc315b2f28
junifar <junifar@gmail.com>
2018-03-27 17:30:54 +0700

    -

--- app/src/main/java/com/rubahapi/ss/HomeActivity.kt
+++ app/src/main/java/com/rubahapi/ss/HomeActivity.kt
@@ -3,10 +3,10 @@ package com.rubahapi.ss
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(){
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
