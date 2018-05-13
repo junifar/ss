1801f21fe141dcb67b00f6d160f598d200c22bf9
junifar <junifar@gmail.com>
2018-05-14 03:26:30 +0700

    xx

--- app/src/main/java/com/rubahapi/ss/HomeActivity.kt
+++ app/src/main/java/com/rubahapi/ss/HomeActivity.kt
@@ -2,11 +2,17 @@ package com.rubahapi.ss
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        btn_test.setOnClickListener {
+            Toast.makeText(this, "Sample", Toast.LENGTH_LONG).show()
+        }
     }
 }
